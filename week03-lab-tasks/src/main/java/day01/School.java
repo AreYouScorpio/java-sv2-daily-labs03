063efe4fa7e9782bec53f793563d69b416a8f7b9
AreYouScorpio <akos.schweger@gmail.com>
2021-10-25 23:56:42 +0200

    setup wk3

--- week03-lab-tasks/src/main/java/day01/School.java
+++ week03-lab-tasks/src/main/java/day01/School.java
@@ -1,4 +1,26 @@
 package day01;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class School {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new  Scanner(System.in);
+Students students = new Students();
+
+
+        int height = scanner.nextInt();
+
+
+
+
+
+        System.out.println(students.isHeightsIncreasing());
+    }
+
+
 }
--- week03-lab-tasks/src/main/java/day01/Students.java
+++ week03-lab-tasks/src/main/java/day01/Students.java
@@ -1,4 +1,30 @@
 package day01;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Students {
+
+    List<Integer> height = new ArrayList<>();
+
+
+//    List<Integer> height = Arrays.asList(3,5,7);
+
+
+    public void addHeight(int height)
+    {
+        this.height.add(height);
+    };
+
+public boolean isHeightsIncreasing(){
+    int counter=0;
+    int a= height.size();
+    for (int i=1; i<a ;i++) {
+        if (height.get(i)>height.get(i-1)) {
+            counter++;
+        }
+    };
+    if (counter == height.size()) { return true;} else { return false;}
+}
 }
