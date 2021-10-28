0db92e138de33bb3a99cc44b24f84e9eb7defae2
AreYouScorpio <akos.schweger@gmail.com>
2021-10-28 16:08:55 +0200

    day4 prison-sultan solution

---
+++ week03-lab-tasks/src/main/java/day04/Prison.java
@@ -0,0 +1,24 @@
+package day04;
+
+import java.util.Arrays;
+
+public class Prison {
+
+    private boolean [] cells = new boolean[100];
+    public boolean[] getCells() {
+        return cells;
+    }
+    public void openFreeCells() {
+
+        for (int i = 0; i < cells.length; i++) {
+
+            for (int j=i; j<cells.length; j++)
+            {
+                if((j+1)%(i+1)==0){
+                    cells[j] = !cells[j];
+                }
+            }
+            System.out.println(cells[i]);
+        }
+    }
+}
